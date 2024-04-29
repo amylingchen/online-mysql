74e0c38905656085d4db9e4035c55b48a6f45f4f
amylingchen <38561335+amylingchen@users.noreply.github.com>
2024-04-29 16:40:30 -0500

    Update mysql.js

--- server/core/mysql.js
+++ server/core/mysql.js
@@ -3,7 +3,7 @@ const  pool =mysql.createPool({
     host    :'localhost',
     port    :'3306',
     user    :'root',
-    password:'Init1234!',
+    password:'123456',
     database:'states',
 });
 module.exports.Pool=pool;
