a6af5a54e4cfb1b1dc3ae70daad42cca6d0af306
lingling <linglingchen9611@gmail.com>
2024-04-29 16:23:02 -0500

    update server

--- server/app.js
+++ server/app.js
@@ -2,22 +2,15 @@ const express =require("express");
 const server =express();
 const router=require("./controller/apiController")
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const corsOptions = {
-    origin: "*",
-    optionsSuccessStatus: 200,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Access-Control-Allow-Origin",
-    credentials: true
-}
-server.use("/api",router);
-//get all states to show
-server.use(express.urlencoded({extended:false}));
-server.use(express.json);
 server.use(cors());
+server.use("/api",router);
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 
 server.listen(8085,()=>{
     console.log("server start!");
-    console.log("http://localhost:8085/");
+    console.log("http://127.0.0.1:8085/");
 })
